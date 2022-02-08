3818601bf0d1fb577925d6f80205f6556b70079f
Mtuozzo86 <Mtuozzo86@gmail.com>
2022-02-07 20:09:50 -0500

    commit tree 0537e7bed2781a7a269a52e44d6213ade855b2c1

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+  validates :title, presence: :true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
+  validate :custom
+
+  def custom
+    unless title.include?("Won't Believe" || "Secret" || "Top" || "Guess")
+      errors.add(:title, "invalid title")
+    end
+  end
 end
